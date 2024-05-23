540b477b6eb96adef0cde0ecc70a82aea34caaac
flipper0l <cases_flipper0l@icloud.com>
2024-05-23 09:47:10 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -14,13 +14,13 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(name: "libOCCT",
-                      url: "https://github.com/flipper0l/OCCT/releases/download/7.8.1/occt-V7_8_1.xcframework.zip",
+                      url: "https://github.com/flipper0l/OCCT-SPM/releases/download/7.8.1/occt-V7_8_1.xcframework.zip",
                       checksum: "460836ba6395d1b907d70827b402f5ce13435109cba091b7743fc61da239cd77"),
         .binaryTarget(name: "libOCCT-macos",
-                      url: "https://github.com/flipper0l/OCCT/releases/download/7.8.1/macos-occt-V7_8_1.xcframework.zip",
+                      url: "https://github.com/flipper0l/OCCT-SPM/releases/download/7.8.1/macos-occt-V7_8_1.xcframework.zip",
                       checksum: "32767cd383cc1f05c03fabb45648b6d8d63f71a099ba3fd289f9ee040699b9ea"),
         .binaryTarget(name: "libOCCT-ios",
-                      url: "https://github.com/flipper0l/OCCT/releases/download/7.8.1/ios-occt-V7_8_1.xcframework.zip",
+                      url: "https://github.com/flipper0l/OCCT-SPM/releases/download/7.8.1/ios-occt-V7_8_1.xcframework.zip",
                       checksum: "2260fb57441e433ba8af71647ea10abb15b0ff57a10ff68813f5b681d84677ff")
     ],
     cxxLanguageStandard: .cxx20
